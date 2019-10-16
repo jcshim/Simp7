326e0ec2804cb109e8722705a5a450babc3bc7fa
jcshim <jcshim@andong.ac.kr>
2019-10-16 23:26:25 +0900

    초기 커밋.

---
+++ Simp7.cpp
@@ -0,0 +1,19 @@
+﻿#include<afxwin.h>
+class CMyFrame : public CFrameWnd {
+public:
+	CMyFrame() {
+		Create(NULL, L"ANU Happy!");
+	}
+};
+class CExample : public CWinApp {
+	BOOL InitInstance() {
+		CMyFrame* Frame = new CMyFrame();
+		m_pMainWnd = Frame;
+
+		Frame->ShowWindow(SW_NORMAL);
+		Frame->UpdateWindow();
+
+		return TRUE;
+	}
+};
+CExample theApp;
